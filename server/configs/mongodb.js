efccaf4e81b8fdbf44fdb4c69c4d0db9f25e588b
sajwansarthak <sarthaksajwan25@gmail.com>
2026-03-26 16:39:58 +0530

    Fixed the database connection in mongodb.js

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -7,7 +7,7 @@ const connectdb = async () =>{
     mongoose.connection.on('connected',() => console.log('Database Connected'))
 
     // connecting to our database ggetting it from environment variable .env and /adding our project name for db
-    await mongoose.connect(`${process.env.MONGODB_URL}`)
+    await mongoose.connect(`${process.env.MONGODB_URL}/lms`)
 }
 // exporting it so that it can be used anywhere in the project
 // Now we need to call this function in our server file so that whenever we start the server db connects automatically 
