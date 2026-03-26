e6daa0232c31fc8ea01f81ecf9be110072921e98
sajwansarthak <sarthaksajwan25@gmail.com>
2026-03-26 16:52:54 +0530

    fixing bugs in  webhook.js added json.stringify again

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -11,7 +11,7 @@ export const clerkWebhooks = async (req,res) =>{
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
         //verify the headers
-        await whook.verify(req.body,{
+        await whook.verify(JSON.stringify(req.body),{
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
