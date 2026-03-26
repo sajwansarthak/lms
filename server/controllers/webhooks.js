2f26a828280cfa6a28ee0b33a724974f96f52316
sajwansarthak <sarthaksajwan25@gmail.com>
2026-03-26 16:20:00 +0530

    Checking for errors

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,24 +1,27 @@
 import {Webhook} from 'svix';
 import User from '../models/user.js';
 
+
 //API controller function to manage clerk user with database
 
 
 export const clerkWebhooks = async (req,res) =>{
+    console.log("🔥 WEBHOOK HIT");
     try{
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
         //verify the headers
-        await whook.verify(req.body),{
+        await whook.verify(req.body,{
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
-        }
+        })
 
         const {data,type} = req.body
 
         switch(type){
             case 'user.created':{
+                console.log("👉 Creating user:", data.id);
                 const userData = {
                     _id: data.id,
                     email: data.email_addresses[0].email_address,
