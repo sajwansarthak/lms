6b7fa0dca1665a66582ba5264840bd81305c1594
sajwansarthak <sarthaksajwan25@gmail.com>
2026-03-30 18:50:07 +0530

    Added api key secret key from cloudinary dashboard

---
+++ server/configs/cloudinary.js
@@ -0,0 +1,11 @@
+import {v2 as cloudinary} from 'cloudinary'
+
+//Function to connect to cloudinary
+const connectCloudinary = async () => {
+    //here we have to provide keys
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET_KEY,
+    })
+}
