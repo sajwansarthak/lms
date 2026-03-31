b1c3c965bfa47c3a86736ccee3ec258323c04c68
sajwansarthak <sarthaksajwan25@gmail.com>
2026-03-31 13:36:52 +0530

    Created Purchase Model

---
+++ server/models/purchase.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const purchaseSchema = new mongoose.Schema({
+    courseId:{type:mongoose.Schema.Types.ObjectId,
+        ref:'Courses',
+        required: true,
+    },
+    userId:{
+        type: String,
+        ref: 'User',
+        required: true,
+    },
+    amount: {type:Number,required: true},
+    status: {type:String, enum: ['Pending,Completed,Failed'],default: 'Pending'},
+
+},{timestamps: true})
+
+export const Purchase = mongoose.model('Purchase',purchaseSchema)
