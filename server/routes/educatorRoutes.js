8d5ff4682258a46c8ef707f2f262f344a867ab81
sajwansarthak <sarthaksajwan25@gmail.com>
2026-04-05 14:52:28 +0530

    Added routes for Educator dashboarddata and enrolled students data

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 //import the educatorController function you have created that is updateRoleToEducator
-import { addCourse, getEducatorCourses, updateRoleToEducator } from '../controllers/educatorControllers.js';
+import { addCourse, getEducatorCourses, getEducatorDashboardData, getEnrolledStudentsData, updateRoleToEducator } from '../controllers/educatorControllers.js';
 import upload from '../configs/multer.js';
 import { protectEducator } from '../middleware/authMiddleware.js';
 
@@ -13,4 +13,8 @@ educatorRouter.get('/update-role',updateRoleToEducator)
 educatorRouter.post('/add-course',upload.single('image'),protectEducator,addCourse)
 //Get AllCourses by a particular educatorId
 educatorRouter.get('/courses',protectEducator,getEducatorCourses)
+//Get Educator dashboard data
+educatorRouter.get('/dashboard',protectEducator,getEducatorDashboardData)
+//Get Enrolled Students Data
+educatorRouter.get('/enrolled-students',protectEducator,getEnrolledStudentsData)
 export default educatorRouter //After exporting it we will provide it in our server.js file
