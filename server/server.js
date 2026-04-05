715b4b98f68be8b785ab8787a8a1b1b151d35035
sajwansarthak <sarthaksajwan25@gmail.com>
2026-04-06 00:14:06 +0530

    Added stripe gateway completely

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express  from "express";
 import cors from "cors";
 import 'dotenv/config'
 import connectdb from "./configs/mongodb.js";
-import { clerkWebhooks } from "./controllers/webhooks.js";
+import { clerkWebhooks, stripeWebhook } from "./controllers/webhooks.js";
 import educatorRouter from "./routes/educatorRoutes.js";
 import {clerkMiddleware} from '@clerk/express'
 import connectCloudinary from "./configs/cloudinary.js";
@@ -40,6 +40,8 @@ app.use('/api/educator',express.json(), educatorRouter)
 app.use('/api/course',express.json(),courseRouter)
 //User Router
 app.use('/api/user',express.json(),userRouter)
+//Stripe 
+app.post('/stripe',express.raw({type: 'application/json'}),stripeWebhook)
 
 
 //Port 
