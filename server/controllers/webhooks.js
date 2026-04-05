8586fec45813a78d2efd066fa23876a7202a7f0c
sajwansarthak <sarthaksajwan25@gmail.com>
2026-04-06 00:07:48 +0530

    Added the stripe webhook function

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,5 +1,8 @@
 import {Webhook} from 'svix';
 import User from '../models/user.js';
+import Stripe from 'stripe';
+import { Purchase } from '../models/purchase.js';
+import Course from '../models/course.js';
 
 
 //API controller function to manage clerk user with database
@@ -58,3 +61,74 @@ export const clerkWebhooks = async (req,res) =>{
     }
 }
 // now we'll go in server and route it
+
+
+//Stripe Webhook
+
+const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET
+
+export const stripeWebhook = async (request, response) => {
+    const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = Stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  }
+  catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':{
+      const paymentIntent = event.data.object;
+      const paymentIntentId = paymentIntent.id
+
+      //puchase Session 
+      const session = await stripeInstance.checkout.sessions.list({
+        payment_intent: paymentIntentId
+      })
+      //Now from this session we will extract purchaseId
+      const {purchaseId} = session.data[0].metadata;
+      //Now using this purchaseId we will find purchaseData 
+      const purchaseData = await Purchase.findById(purchaseId)
+      //Now we will find userData using the purchaseData
+      const userData = await User.findById(purchaseData.userId)
+      //Now we will find courseData using purchaseData
+      const courseData = await Course.findById(purchaseData.courseId.toString())
+
+      //Now we have to add the userData in courseData and courseData in userData
+      courseData.enrolledStudents.push(userData) //now saving it in mongodb
+      await courseData.save()
+      userData.enrolledCourses.push(courseData._id)
+      await userData.save()
+
+      //changing purchase status
+      purchaseData.status = 'Completed'
+      await purchaseData.save()
+      break;}
+    case 'payment_intent.payment_failed':{
+      const paymentIntent = event.data.object;
+      const paymentIntentId = paymentIntent.id
+
+      //puchase Session 
+      const session = await stripeInstance.checkout.sessions.list({
+        payment_intent: paymentIntentId
+      })
+      //Now from this session we will extract purchaseId
+      const {purchaseId} = session.data[0].metadata;
+
+      const purchaseData = await Purchase.findById(purchaseId)
+      //Now we will update the purchase status to failed 
+      purchaseData.status ='Failed'
+      await purchaseData.save()
+      break;}
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+  // Return a response to acknowledge receipt of the event
+  response.json({received: true});
+}
