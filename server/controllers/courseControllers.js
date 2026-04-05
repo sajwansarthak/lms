6b4903fc5a75878a6fd62715235e6e660a6d6072
sajwansarthak <sarthaksajwan25@gmail.com>
2026-04-05 16:28:28 +0530

    Fixed bugs in CourseController and added CourseRoute in main serverfile

--- server/controllers/courseControllers.js
+++ server/controllers/courseControllers.js
@@ -5,7 +5,7 @@ import Course from "../models/course.js"
 export const getAllCourses = async (req,res) =>{
     try{
         // If any course is published then only we will display it on webpage we use populate educator so that we get all the educator details to display on our frontend
-        const courses = await Course.find({isPublished: true}).select(['-courseContent','-enrolledStudents']).populate({path:'educator '})
+        const courses = await Course.find({isPublished: true}).select(['-courseContent','-enrolledStudents']).populate({path:"educator"})
 
         res.json({success: true, courses})
     }catch(error){
@@ -17,7 +17,7 @@ export const getAllCourses = async (req,res) =>{
 export const getCourseById = async (req,res) =>{
     const {id} = req.params
     try{
-        const courseData = await Course.findById(id).populate({path:'educator'})
+        const courseData = await Course.findById(id).populate({path:"educator"})
         //Here we will get all the chapters and lectures of the individual course but from that we have to remove chapter url if preview is not free 
         //Remove lectureUrl if idPreview is false
         courseData.courseContent.forEach(chapter => {
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { clerkWebhooks } from "./controllers/webhooks.js";
 import educatorRouter from "./routes/educatorRoutes.js";
 import {clerkMiddleware} from '@clerk/express'
 import connectCloudinary from "./configs/cloudinary.js";
+import courseRouter from "./routes/courseRoutes.js";
 
 
 //Initializing Express
@@ -34,6 +35,8 @@ app.get('/', (req,res) =>{
 app.post('/clerk', express.json(), clerkWebhooks)
 //Educator Route from routes => educatorRoutes
 app.use('/api/educator',express.json(), educatorRouter)
+//Course Route
+app.use('/api/course',express.json(),courseRouter)
 
 
 //Port 
