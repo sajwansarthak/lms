4cd3774cf118cb3e0907bef3bd5dabe7b20c8a5a
sajwansarthak <sarthaksajwan25@gmail.com>
2026-04-16 04:22:11 +0530

    Made the myEnrollment page connect with backend

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -1,31 +1,61 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState,useEffect } from 'react'
 import { AppContext } from '../../context/AddContext'
 import {Line} from 'rc-progress'
 import Footer from '../../components/student/Footer'
+import {toast} from 'react-toastify'
+import axios from 'axios'
 
 //Using the package rc-progress which we have already downloaded we will display the progress bat but first we have to import it 
 
 function MyEnrollments() {
 
-  const {enrolledCourses,calculateCourseDuration,navigate} = useContext(AppContext)
+  const {enrolledCourses,calculateCourseDuration,navigate,userData,fetchUserEnrolledCourses,backendUrl,getToken,calculateNoOfLectures} = useContext(AppContext)
 
   //Adding progress bar in the table
-  const [progressArray] = useState([
-    { lectureCompleted: 2, totalLectures: 4 },
-    { lectureCompleted: 1, totalLectures: 5 },
-    { lectureCompleted: 3, totalLectures: 6 },
-    { lectureCompleted: 4, totalLectures: 4 },
-    { lectureCompleted: 0, totalLectures: 3 },
-    { lectureCompleted: 5, totalLectures: 7 },
-    { lectureCompleted: 6, totalLectures: 8 },
-    { lectureCompleted: 2, totalLectures: 6 },
-    { lectureCompleted: 4, totalLectures: 10 },
-    { lectureCompleted: 3, totalLectures: 5 },
-    { lectureCompleted: 7, totalLectures: 7 },
-    { lectureCompleted: 1, totalLectures: 4 },
-    { lectureCompleted: 0, totalLectures: 2 },
-    { lectureCompleted: 5, totalLectures: 5 }
+  const [progressArray,setProgressArray] = useState([
+    //TO check the frontend 
+    // { lectureCompleted: 2, totalLectures: 4 },
+    // { lectureCompleted: 1, totalLectures: 5 },
+    // { lectureCompleted: 3, totalLectures: 6 },
+    // { lectureCompleted: 4, totalLectures: 4 },
+    // { lectureCompleted: 0, totalLectures: 3 },
+    // { lectureCompleted: 5, totalLectures: 7 },
+    // { lectureCompleted: 6, totalLectures: 8 },
+    // { lectureCompleted: 2, totalLectures: 6 },
+    // { lectureCompleted: 4, totalLectures: 10 },
+    // { lectureCompleted: 3, totalLectures: 5 },
+    // { lectureCompleted: 7, totalLectures: 7 },
+    // { lectureCompleted: 1, totalLectures: 4 },
+    // { lectureCompleted: 0, totalLectures: 2 },
+    // { lectureCompleted: 5, totalLectures: 5 }
+    //Getting data from backend
+
   ])
+  //Function to get Course Progress Data
+  const getCourseProgress = async () =>{
+    try{
+      const token = await getToken()
+      const tempProgressArray = await Promise.all(
+        enrolledCourses.map(async (course) =>{
+          const {data} = await axios.post (`${backendUrl}/api/user/get-course-progress`,{courseId: course._id},{headers: {Authorization:`Bearer ${token}`}})
+          let totalLectures = calculateNoOfLectures(course)
+          const lectureCompleted = data.progressData ? data.progressData.lectureCompleted.length : 0;
+          return {totalLectures,lectureCompleted}
+        })
+      )
+      setProgressArray(tempProgressArray)
+    }catch(error){
+      toast.error(error.message)
+    }
+  }
+  useEffect(() =>{
+    if(userData){
+      fetchUserEnrolledCourses()
+    }
+  },[userData])
+  useEffect(() =>{
+    getCourseProgress()
+  },[enrolledCourses])
   return (
     <>
     <div className='md:px-36 px-8 pt-10'>
